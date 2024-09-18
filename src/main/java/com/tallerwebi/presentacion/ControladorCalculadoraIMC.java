368182584592de1bd436a4116338130c879e5f21
guido <guidofink01@gmail.com>
2024-09-18 15:09:45 -0300

    funcionalidad de calculadoraIMC

--- src/main/java/com/tallerwebi/presentacion/ControladorCalculadoraIMC.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorCalculadoraIMC.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,4 +15,57 @@ public class ControladorCalculadoraIMC {
 
     public ModelAndView mostrarCalculadora() { return new ModelAndView("calculadoraIMC.html");}
 
+    @RequestMapping(path= "/calcularIMC")
+    public ModelAndView calcularIMC (@RequestParam String genero, @RequestParam double altura, @RequestParam double peso, @RequestParam Integer edad){
+
+        //paso altura a metros
+        altura = altura / 100;
+
+        //calculo el IMC
+        double imc = peso / (altura * altura);
+
+        //Clasifico en hombre o mujer
+        String clasificacion;
+
+        if (genero.equalsIgnoreCase("hombre")) {
+            // clasificacion especifica para hombres
+            if (imc < 20) {
+                clasificacion = "Bajo peso";
+            } else if (imc < 25) {
+                clasificacion = "Normal";
+            } else if (imc < 30) {
+                clasificacion = "Sobrepeso";
+            } else {
+                clasificacion = "Obesidad";
+            }
+        } else {
+            // clasificacion especifica para mujeres
+            if (imc < 19) {
+                clasificacion = "Bajo peso";
+            } else if (imc < 24) {
+                clasificacion = "Normal";
+            } else if (imc < 29) {
+                clasificacion = "Sobrepeso";
+            } else {
+                clasificacion = "Obesidad";
+            }
+        }
+
+
+        //agrego resultados al modelo
+        ModelMap modelo = new ModelMap();
+
+        modelo.put("clasificacion", clasificacion);
+        modelo.put("genero",genero);
+        modelo.put("altura",altura);
+        modelo.put("peso",peso);
+        modelo.put("edad",edad);
+        modelo.put("imc",imc);
+
+
+
+        //redirigo a la pagina
+        return new ModelAndView("resultadoIMC", modelo);
+    }
 }
+
