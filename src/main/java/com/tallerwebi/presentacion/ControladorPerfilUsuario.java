9059151a2e80cbb73b279002d622c1e37aee77a8
Javier <cabreramoreljavier@gmail.com>
2024-11-06 13:31:17 -0300

    Boton de comprar desaparesca si ya es premium

--- src/main/java/com/tallerwebi/presentacion/ControladorPerfilUsuario.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfilUsuario.java
@@ -15,66 +15,79 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ControladorPerfilUsuario {
-    
+
     private ServicioPerfilUsuario servicioPerfilUsuario;
-    
+
     @Autowired
-    
-    public void ControladorPerfilUsuario(ServicioPerfilUsuario servicioPerfilUsuario){
-    this.servicioPerfilUsuario=servicioPerfilUsuario;
+
+    public void ControladorPerfilUsuario(ServicioPerfilUsuario servicioPerfilUsuario) {
+        this.servicioPerfilUsuario = servicioPerfilUsuario;
     }
 
     @RequestMapping(path = "/perfilusuario", method = RequestMethod.GET)
     public ModelAndView mostrarDatosDelUsuario(HttpServletRequest request) {
-        if (request.getSession().getAttribute("EMAIL") == null) {
+
+        String email = (String) request.getSession().getAttribute("EMAIL");
+
+        if (email == null) {
             return new ModelAndView("redirect:/login");
         }
-        String email = (String) request.getSession().getAttribute("EMAIL");
-        Usuario usuarioBuscado=servicioPerfilUsuario.buscarUsuarioPoreEmail(email);
+        Usuario usuarioBuscado = servicioPerfilUsuario.buscarUsuarioPoreEmail(email);
 
-        ModelMap mavUsuario=new ModelMap();
+        ModelMap mavUsuario = new ModelMap();
 
-        mavUsuario.put("nombre",usuarioBuscado.getNombre());
-        mavUsuario.put("apellido",usuarioBuscado.getApellido());
-        mavUsuario.put("objetivoSalud",usuarioBuscado.getObjetivoSalud());
-        mavUsuario.put("preferenciaAlimenticia",usuarioBuscado.getPreferenciaAlimenticia());
-        mavUsuario.put("restrincionesAlimentarias",usuarioBuscado.getRestrincionesAlimentarias());
-        mavUsuario.put("informacionAdicional",usuarioBuscado.getInformacionAdicional());
 
-        return new ModelAndView( "perfilusuario",mavUsuario);
+
+
+        if (usuarioBuscado != null && usuarioBuscado.getEsPremium() != null && usuarioBuscado.getEsPremium()) {
+            mavUsuario.put("esPremium", true);
+        } else {
+            mavUsuario.put("esPremium", false);
+        }
+
+
+
+        mavUsuario.put("nombre", usuarioBuscado.getNombre());
+        mavUsuario.put("apellido", usuarioBuscado.getApellido());
+        mavUsuario.put("objetivoSalud", usuarioBuscado.getObjetivoSalud());
+        mavUsuario.put("preferenciaAlimenticia", usuarioBuscado.getPreferenciaAlimenticia());
+        mavUsuario.put("restrincionesAlimentarias", usuarioBuscado.getRestrincionesAlimentarias());
+        mavUsuario.put("informacionAdicional", usuarioBuscado.getInformacionAdicional());
+
+        return new ModelAndView("perfilusuario", mavUsuario);
     }
 
 
     @RequestMapping(path = "/mostrarFormulario", method = RequestMethod.GET)
     public ModelAndView mostrarFormulario() {
 
-        ModelMap map=new ModelMap();
-        map.put("usuario",new Usuario());
-        return new ModelAndView("formulario",map);
+        ModelMap map = new ModelMap();
+        map.put("usuario", new Usuario());
+        return new ModelAndView("formulario", map);
     }
 
     @RequestMapping(path = "/enviarDatos", method = RequestMethod.POST)
     public ModelAndView enviarDatosDelFormulario(@ModelAttribute("usuario") Usuario usuario, HttpServletRequest request) {
 
         String email = (String) request.getSession().getAttribute("EMAIL");
-        Usuario usuarioActivo=servicioPerfilUsuario.buscarUsuarioPoreEmail(email);
+        Usuario usuarioActivo = servicioPerfilUsuario.buscarUsuarioPoreEmail(email);
 
-        Usuario usuarioActualizado=servicioPerfilUsuario.modificarUsuario(usuarioActivo , usuario);
+        Usuario usuarioActualizado = servicioPerfilUsuario.modificarUsuario(usuarioActivo, usuario);
         ModelMap model = new ModelMap();
-        model.put("nombre",usuarioActualizado.getNombre());
-        model.put("apellido",usuarioActualizado.getApellido());
-        model.put("objetivoSalud",usuarioActualizado.getObjetivoSalud());
-        model.put("preferenciaAlimenticia",usuarioActualizado.getPreferenciaAlimenticia());
-        model.put("restrincionesAlimentarias",usuarioActualizado.getRestrincionesAlimentarias());
-        model.put("informacionAdicional",usuarioActualizado.getInformacionAdicional());
+        model.put("nombre", usuarioActualizado.getNombre());
+        model.put("apellido", usuarioActualizado.getApellido());
+        model.put("objetivoSalud", usuarioActualizado.getObjetivoSalud());
+        model.put("preferenciaAlimenticia", usuarioActualizado.getPreferenciaAlimenticia());
+        model.put("restrincionesAlimentarias", usuarioActualizado.getRestrincionesAlimentarias());
+        model.put("informacionAdicional", usuarioActualizado.getInformacionAdicional());
 
-        return new ModelAndView("perfilusuario",model);
+        return new ModelAndView("perfilusuario", model);
 
     }
 
     @RequestMapping(path = "/calcularMacro", method = RequestMethod.GET)
-     public ModelAndView mostrarVistaCorrecta() {
-      ModelAndView model=new ModelAndView("calculadoraIMC");
-         return model;
+    public ModelAndView mostrarVistaCorrecta() {
+        ModelAndView model = new ModelAndView("calculadoraIMC");
+        return model;
     }
 }
